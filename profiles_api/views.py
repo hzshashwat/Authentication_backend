08e31a4822edd5823ebcd60138b5d10c38216843
Shashwat Singh <shashwatsingh741@gmail.com>
2022-11-13 23:28:06 +0530

    Signup API built

--- profiles_api/views.py
+++ profiles_api/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from profiles_api import models, serializers
+from rest_framework import viewsets
 
 # Create your views here.
+class UserProfileViewSet(viewsets.ModelViewSet):
+    """Handle creating, creating and updating profiles"""
+    serializer_class = serializers.UserProfileSerializer
+    queryset = models.UserProfile.objects.all()
