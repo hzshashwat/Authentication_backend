60ed49d5497100c39f1770eef22ffb007e0339af
Shashwat Singh <shashwatsingh741@gmail.com>
2022-11-14 00:40:35 +0530

    User profile update permissions added

--- profiles_api/views.py
+++ profiles_api/views.py
@@ -1,9 +1,16 @@
 from django.shortcuts import render
-from profiles_api import models, serializers
-from rest_framework import viewsets
+from profiles_api import models, serializers, permissions
+from rest_framework import viewsets, filters
+from rest_framework.authentication import TokenAuthentication
+
 
 # Create your views here.
+
 class UserProfileViewSet(viewsets.ModelViewSet):
     """Handle creating, creating and updating profiles"""
     serializer_class = serializers.UserProfileSerializer
     queryset = models.UserProfile.objects.all()
+    authentication_classes = (TokenAuthentication, )
+    permission_classes = (permissions.UpdateOwnProfile, )
+    filter_backends = (filters.SearchFilter, )
+    search_fields = ('name', 'email', )
