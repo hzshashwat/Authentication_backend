257b9c25c79c2afc84d75a1c442e9df74ba5a50d
Shashwat Singh <shashwatsingh741@gmail.com>
2022-11-16 09:09:02 +0530

    Password-Reset framework removed

--- profiles_api/models.py
+++ profiles_api/models.py
@@ -53,24 +53,3 @@ class UserProfile(AbstractBaseUser, PermissionsMixin):
         """Return string representation of user"""
         return self.email
 
-from django.dispatch import receiver
-from django.urls import reverse
-from django_rest_passwordreset.signals import reset_password_token_created
-from django.core.mail import send_mail  
-
-
-@receiver(reset_password_token_created)
-def password_reset_token_created(sender, instance, reset_password_token, *args, **kwargs):
-
-    email_plaintext_message = "http://127.0.0.1:8000{}?token={}".format(reverse('password_reset:reset-password-request'), reset_password_token.key)
-
-    send_mail(
-        # title:
-        "Password Reset Link",
-        # message:
-        email_plaintext_message,
-        # from:
-        "noreply@somehost.local",
-        # to:
-        [reset_password_token.user.email]
-    )
--- profiles_api/urls.py
+++ profiles_api/urls.py
@@ -9,7 +9,6 @@ urlpatterns = [
     path('', include(router.urls)),
     path('nickname/', views.NicknameAPIView.as_view()),
     path('login/', views.UserLoginApiView.as_view()),
-    path('password/reset/', include('django_rest_passwordreset.urls', namespace='password_reset')),
     path('admin/delete/', views.DeleteUserAPIView.as_view()),
     path('admin/make_admin/', views.MakeAdminAPIView.as_view())
 ]
