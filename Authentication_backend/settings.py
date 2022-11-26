9d1000848f5639dc80df836c7a6d6b04f2f8d84c
Shashwat Singh <shashwatsingh741@gmail.com>
2022-11-26 20:22:55 +0530

    requirements.txt added and allowed hosts added

--- Authentication_backend/settings.py
+++ Authentication_backend/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-!0sgkhyk2z$=^^^xrl-2+66&nixmylei9l)i+02mq%ek&bdh4u'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1', '18.183.63.226']
 
 
 # Application definition
